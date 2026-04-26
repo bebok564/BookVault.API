8e8cc19b35f9ea0d3e9e0bed086e02bf5b32b90f
Natalia <nataliaszolc.kontakt@gmail.com>
2026-04-26 21:39:00 +0200

    feat: add custom middleware

---
+++ BookVault.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,38 @@
+﻿namespace BookVault.API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+            private readonly RequestDelegate _next;
+            private readonly ILogger<ExceptionMiddleware> _logger;
+
+            public ExceptionMiddleware(RequestDelegate next,
+                ILogger<ExceptionMiddleware> logger)
+            {
+                _next = next;
+                _logger = logger;
+            }
+
+            public async Task InvokeAsync(HttpContext context)
+            {
+                try
+                {
+                    await _next(context);
+                }
+                catch (KeyNotFoundException ex)
+                {
+                    _logger.LogWarning(ex, "Resource not found");
+                    context.Response.StatusCode = 404;
+                    await context.Response.WriteAsJsonAsync(
+                        new { error = ex.Message });
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Unhandled exception");
+                    context.Response.StatusCode = 500;
+                    await context.Response.WriteAsJsonAsync(
+                        new { error = "Something went wrong" });
+                }
+            }
+        
+    }
+}
